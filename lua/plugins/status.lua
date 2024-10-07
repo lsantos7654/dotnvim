532d4f19243eeb59b2b8a4841082f9d7acc52486
santos <lsantos7654@gmail.com>
2024-10-06 23:50:48 -0400

    fixed status.lua

--- lua/plugins/status.lua
+++ lua/plugins/status.lua
@@ -39,97 +39,60 @@ return {
 				hl = function()
 					return {
 						fg = require("feline.providers.vi_mode").get_mode_color(),
-						bg = "darkblue",
+						bg = "NONE",
 						style = "bold",
 						name = "NeovimModeHLColor",
 					}
 				end,
-				left_sep = "block",
-				right_sep = "block",
+				left_sep = " ",
+				right_sep = " ",
 			},
 			gitBranch = {
 				provider = "git_branch",
 				hl = {
 					fg = "peanut",
-					bg = "darkblue",
-					style = "bold",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			gitDiffAdded = {
-				provider = "git_diff_added",
-				hl = {
-					fg = "green",
-					bg = "darkblue",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			gitDiffRemoved = {
-				provider = "git_diff_removed",
-				hl = {
-					fg = "red",
-					bg = "darkblue",
-				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			gitDiffChanged = {
-				provider = "git_diff_changed",
-				hl = {
-					fg = "fg",
-					bg = "darkblue",
-				},
-				left_sep = "block",
-				right_sep = "right_filled",
-			},
-			separator = {
-				provider = "",
-			},
-			fileinfo = {
-				provider = {
-					name = "file_info",
-					opts = {
-						type = "relative-short",
-					},
-				},
-				hl = {
+					bg = "NONE",
 					style = "bold",
 				},
 				left_sep = " ",
 				right_sep = " ",
 			},
-			diagnostic_errors = {
-				provider = "diagnostic_errors",
+			gitDiffAdded = {
+				provider = "git_diff_added",
+				hl = {
+					fg = "green",
+					bg = "NONE",
+				},
+				left_sep = " ",
+				right_sep = " ",
+			},
+			gitDiffRemoved = {
+				provider = "git_diff_removed",
 				hl = {
 					fg = "red",
+					bg = "NONE",
 				},
+				left_sep = " ",
+				right_sep = " ",
 			},
-			diagnostic_warnings = {
-				provider = "diagnostic_warnings",
+			gitDiffChanged = {
+				provider = "git_diff_changed",
 				hl = {
-					fg = "yellow",
+					fg = "fg",
+					bg = "NONE",
 				},
-			},
-			diagnostic_hints = {
-				provider = "diagnostic_hints",
-				hl = {
-					fg = "aqua",
-				},
-			},
-			diagnostic_info = {
-				provider = "diagnostic_info",
+				left_sep = " ",
+				right_sep = " ",
 			},
 			lsp_client_names = {
 				provider = "lsp_client_names",
 				hl = {
 					fg = "purple",
-					bg = "darkblue",
+					bg = "NONE",
 					style = "bold",
 				},
-				left_sep = "left_filled",
-				right_sep = "block",
+				left_sep = " ",
+				right_sep = " ",
 			},
 			file_type = {
 				provider = {
@@ -141,48 +104,31 @@ return {
 				},
 				hl = {
 					fg = "red",
-					bg = "darkblue",
+					bg = "NONE",
 					style = "bold",
 				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			file_encoding = {
-				provider = "file_encoding",
-				hl = {
-					fg = "orange",
-					bg = "darkblue",
-					style = "italic",
-				},
-				left_sep = "block",
-				right_sep = "block",
+				left_sep = " ",
+				right_sep = " ",
 			},
 			position = {
 				provider = "position",
 				hl = {
 					fg = "green",
-					bg = "darkblue",
+					bg = "NONE",
 					style = "bold",
 				},
-				left_sep = "block",
-				right_sep = "block",
+				left_sep = " ",
+				right_sep = " ",
 			},
 			line_percentage = {
 				provider = "line_percentage",
 				hl = {
 					fg = "aqua",
-					bg = "darkblue",
+					bg = "NONE",
 					style = "bold",
 				},
-				left_sep = "block",
-				right_sep = "block",
-			},
-			scroll_bar = {
-				provider = "scroll_bar",
-				hl = {
-					fg = "yellow",
-					style = "bold",
-				},
+				left_sep = " ",
+				right_sep = " ",
 			},
 		}
 
@@ -192,35 +138,24 @@ return {
 			c.gitDiffAdded,
 			c.gitDiffRemoved,
 			c.gitDiffChanged,
-			c.separator,
-		}
-
-		local middle = {
-			c.fileinfo,
-			c.diagnostic_errors,
-			c.diagnostic_warnings,
-			c.diagnostic_info,
-			c.diagnostic_hints,
 		}
 
 		local right = {
 			c.lsp_client_names,
 			c.file_type,
-			c.file_encoding,
 			c.position,
 			c.line_percentage,
-			c.scroll_bar,
 		}
 
 		local components = {
 			active = {
 				left,
-				middle,
+				{}, -- Empty middle section
 				right,
 			},
 			inactive = {
 				left,
-				middle,
+				{}, -- Empty middle section
 				right,
 			},
 		}
