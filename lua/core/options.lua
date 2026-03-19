d446765065d8ca56ddbbb98a9eb14379e287cba6
Lucas Santos <lsantos7654@gmail.com>
2026-03-18 22:33:29 -0400

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -162,7 +162,7 @@ end
 
 -- 📁 Folding configuration
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext =
 	[[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
 vim.opt.fillchars = {
@@ -171,7 +171,7 @@ vim.opt.fillchars = {
 	vert = "│", -- Vertical split character
 }
 vim.opt.foldcolumn = "0"
-vim.opt.foldenable = false
+vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldnestmax = 3
